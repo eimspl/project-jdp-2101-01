f90bb16b70399c93bf3a11c5aedb659127526cdc
Krzysztof Chwistek <vescus@gmail.com>
2021-01-29 11:06:13 +0100

    mysql connector -> build gradle, wstepna implementacja endpointow

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.OrderDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/order")
 public class OrderController {
+
+    @GetMapping("/showAllOrders")
+    public List<OrderDto> showAllOrders(){
+        return new ArrayList<>();
+    }
+
+    @PostMapping("/addNewOrder")
+    public void addNewOrder(OrderDto orderDto){
+
+    }
+
+    @GetMapping("/showOrder/{orderId}")
+    public OrderDto showOrder(Long id){
+        return new OrderDto(1L, "test_order");
+    }
+
+    @PutMapping("/updateOrder/{orderId}")
+    public OrderDto updateOrder(OrderDto orderDto){
+            return new OrderDto(1L, "test order edited");
+    }
+
+    @DeleteMapping("/removeOrder/{orderId}")
+    public void removeOrder(Long orderId){
+    }
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -0,0 +1,6 @@
+package com.kodilla.ecommercee.domain;
+
+public class Order {
+    private Long orderId;
+    private String orderName;
+}
