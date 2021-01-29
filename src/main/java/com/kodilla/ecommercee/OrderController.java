50617d534038ae25db48000c21f81c97c5fd30e0
Krzysztof Chwistek <vescus@gmail.com>
2021-01-29 17:24:57 +0100

    Dodane adnotacje

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -6,32 +6,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/order")
 public class OrderController {
 
     @GetMapping("/showAllOrders")
-    public List<OrderDto> showAllOrders(){
+    public List<OrderDto> showAllOrders() {
         return new ArrayList<>();
     }
 
     @PostMapping("/addNewOrder")
-    public void addNewOrder(OrderDto orderDto){
+    public void addNewOrder(@RequestBody OrderDto orderDto) {
 
     }
 
     @GetMapping("/showOrder/{orderId}")
-    public OrderDto showOrder(Long id){
+    public OrderDto showOrder(@PathVariable("orderId") Long orderId) {
         return new OrderDto(1L, "test_order");
     }
 
     @PutMapping("/updateOrder/{orderId}")
-    public OrderDto updateOrder(OrderDto orderDto){
-            return new OrderDto(1L, "test order edited");
+    public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
+        return new OrderDto(1L, "test order edited");
     }
 
     @DeleteMapping("/removeOrder/{orderId}")
-    public void removeOrder(Long orderId){
+    public void removeOrder(@RequestParam("orderId") Long orderId) {
     }
 
 }
