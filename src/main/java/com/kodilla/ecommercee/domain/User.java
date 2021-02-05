f2e10298f5844a3d914726bc8ed13cffaa34d3f2
Marcin Łukasik <marcin@eims.pl>
2021-02-05 17:19:12 +0100

    JDP210101-15/ zmiana konceptu relacji - aktualizacja encji

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -42,18 +42,12 @@ public class User {
     @Column(name = "PHONE_NUMBER")
     private String phoneNumber;
 
-    /* @OneToMany(                       //potrzebuje encji order - chyba już jest...
-            targetEntity = Order.class,
+    /* @OneToMany(                       //encja koszyk zostanie przygotowana przez kolegę - Krzyśka, wtedy złączymy.
+            targetEntity = Carts.class,
             mappedBy = "user",
             cascade = CascadeType.REMOVE,
             fetch = FetchType.LAZY)
-    private List<Order> orders = new ArrayList<>(); */
-
-    /*                                   //potrzebuje encji koszyk
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "CART_ID")
-    private Cart cart = new Cart();
-     */
+    private List<Cart> carts = new ArrayList<>(); */
 
     public User(String userName, String password, String emailAddress) {
         this.userName = userName;
