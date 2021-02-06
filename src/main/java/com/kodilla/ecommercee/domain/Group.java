c2449862a88da4b3263035770bb937c942fe415f
Mateusz Wójciak <matwojciak@gmail.com>
2021-02-06 17:17:38 +0100

    Dodanie encji Group

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,2 +1,36 @@
-package com.kodilla.ecommercee.domain;public class Group {
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "groups")
+public class Group {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    /*    //do zaimplementowania po dodaniu przez inną osobę klasy Product do maina
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = " ",
+            cascade = CasadeType.REMOVE,
+            fetch = FetchType.LAZY)
+    private List<Product> products = new ArrayList();
+     */
 }
