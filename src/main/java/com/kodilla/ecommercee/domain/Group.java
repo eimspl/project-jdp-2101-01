77c71f1a268fe9f0b62d9e93ed5c29b6eff5b0a1
Mateusz Wójciak <matwojciak@gmail.com>
2021-02-07 17:35:36 +0100

    Add Setter in Group

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -3,26 +3,31 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @Entity
-@Table(name = "groups")
+@Table(name = "GROUPS")
 public class Group {
 
     @Id
     @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "NAME")
     private String name;
 
-    @Column(name = "description")
+    @Column(name = "DESCRIPTION")
     private String description;
 
     /*    //do zaimplementowania po dodaniu przez inną osobę klasy Product do maina
